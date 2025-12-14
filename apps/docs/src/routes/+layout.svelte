6e3b7dd94a45ae4e590427940b1a892c84f8fd74
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-12-14 11:40:10 +0100

    feat: add View Transitions

--- apps/docs/src/routes/+layout.svelte
+++ apps/docs/src/routes/+layout.svelte
@@ -4,6 +4,20 @@ import favicon from "$lib/assets/favicon.svg";
 import { VarselToaster } from "varsel";
 
 let { children } = $props();
+
+import { onNavigate } from '$app/navigation';
+
+onNavigate((navigation) => {
+  if (!document.startViewTransition) return;
+
+  return new Promise<void>((resolve) => {
+    document.startViewTransition(async () => {
+      resolve();
+      await navigation.complete;
+    });
+  });
+});
+
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
